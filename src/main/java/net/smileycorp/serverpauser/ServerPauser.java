ccf36e8b7b54c971f8d462c18600a611b67d4e1f
Smileycorp <callum.j.longbottom@gmail.com>
2023-05-01 18:22:03 +0100

    initial commit

---
+++ src/main/java/net/smileycorp/serverpauser/ServerPauser.java
@@ -0,0 +1,8 @@
+package net.smileycorp.serverpauser;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = "serverpauser", acceptableRemoteVersions="*", name="Serverpauser", version = "1.0.0")
+public class ServerPauser {
+
+}
